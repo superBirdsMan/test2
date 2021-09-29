ae9e1b3a77a3bd383339050529f3bc5404db0e83
xjs <774974270@qq.com>
2021-09-29 10:18:25 +0800

    提交1

--- springcloud01eureka_server/src/main/java/com/my/EurekaServerApplication.java
+++ springcloud01eureka_server/src/main/java/com/my/EurekaServerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServerApplication {
     public static void main(String[] args) {
         System.out.println(333);
+        System.out.println(444);
         SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
